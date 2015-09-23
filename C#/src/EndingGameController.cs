41ee244bb2b77be494f5083c6b4b28d699e52da4
Kyle Van Berendonck <kyle@vanberendonck.com>
2015-09-23 18:37:12 +1000

    Fix build

--- C#/src/EndingGameController.cs
+++ C#/src/EndingGameController.cs
@@ -46,7 +46,7 @@ static class EndingGameController
 	{
 		if (SwinGame.MouseClicked (MouseButton.LeftButton) || SwinGame.KeyTyped (KeyCode.vk_RETURN) || SwinGame.KeyTyped (KeyCode.vk_ESCAPE))
 		{
-			HighScoreController.ReadHighScore (AddNewState.HighScoreController.Score);
+			//HighScoreController.ReadHighScore (AddNewState(GameState.ViewingHighScores));
 			GameController.EndCurrentState ();
 		}
 	}
