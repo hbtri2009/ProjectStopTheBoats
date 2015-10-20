4a51810d1995e09772c9321cbc852870133da796
CourtneyShaw <ccjshaw@gmail.com>
2015-10-20 18:58:57 +1100

    testing framework

---
+++ C#/TestsBattleShips/src/Model/TestsTile.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class TestsTile
+    {
+        [TestMethod()]
+        public void TestTile()
+        {
+            Tile test = new Tile(10, 10, new Ship(ShipName.Destroyer));
+
+            Assert.IsFalse(test.Shot);
+
+            test.
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestClearShip()
+        {
+            Assert.Fail();
+        }
+    }
+}
